37f2b46ce4cc5e6288e2da3d84535c5ad7ec5354
kstAtBitLC <klaus.stolz@bitlc.de>
2020-01-28 11:29:50 +0100

    Add project files.

---
+++ MehrfachvererbungMitInterfaces2/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MehrfachvererbungMitInterfaces2 {
+    abstract class Person {
+        public string Name { get; set; }
+    }
+
+    abstract class Bürger: Person {
+        public void SteuernZahlen() {
+            Console.WriteLine ("Ich zahlen gern ;-)");
+        }
+    }
+
+    interface Outlaw {
+        void MalDieMeinungSagen();        
+    }
+
+    class Betrüger:Bürger, Outlaw {
+        public void Betrügen () {
+            Console.WriteLine ("Sie wurden soeben betrogen!");
+        }
+
+        public void MalDieMeinungSagen () {
+            Console.WriteLine ("Sach auch mal - sagt der Betrüger!");
+        }
+    }
+
+    class Kaninchen : Outlaw {
+        public string Kosename { get; set; }
+
+        void Outlaw.MalDieMeinungSagen () {
+            Console.WriteLine ("laß Dir das von einem Kaninchen sagen!") ;
+        }
+    }
+
+}
---
+++ MehrfachvererbungMitInterfaces2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MehrfachvererbungMitInterfaces2 {
+    class Program {
+        static void Main (string [] args) {
+            // geht nicht wegen abstract
+            //Person p = new Person () { Name = "Kurt" };
+
+            // geht nicht wegen abstract
+            //Bürger schild = new Bürger ();
+
+            // geht nicht wegen: Outlaw ist keine Klasse, sondern ein Interface
+            // mit Interfaces lassen sich keine Objekte erzeugen!!!!
+            //Outlaw polizist = new Outlaw ();
+
+            // was immer geht
+            //Betrüger x = new Betrüger ();
+
+            // interessant ;-) Ein Array auf Basis von Outlaw
+            Outlaw[] al = new Outlaw[2];
+            al [ 0 ] = new Kaninchen ();
+            al [ 1 ] = new Betrüger ();
+
+            foreach ( var item in al ) {
+                item.MalDieMeinungSagen ();                
+            }
+
+            Outlaw ol1 = new Betrüger () { Name = "misch" };
+            
+
+            Betrüger b = (Betrüger) ol1;
+            Console.WriteLine(b.Name);
+
+            Outlaw ol2 = new Kaninchen () { Kosename = "Hasi" };
+            Kaninchen k = (Kaninchen) ol2;
+            Console.WriteLine (k.Kosename);
+
+            Console.ReadLine ();
+        }
+    }
+}
